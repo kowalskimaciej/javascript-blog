d7ac94b3d37a99eaf5df8ce989d5adfd70bc22f1
Maciej Kowalski <maciej.kowalski1@proton.me>
2024-04-15 15:41:49 +0200

    Add class active for clickedElement

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
+const { active } = require("browser-sync");
+
 const titleClickHandler = function(event){
+      const clickedElement = this;
       console.log('Link was clicked!');
-      console.log(event);
-      /* remove class 'active' from all article links  */
+    
+      /* [DONE] remove class 'active' from all article links  */
+    
       const activeLinks = document.querySelectorAll('.titles a.active');
-
+    
       for(let activeLink of activeLinks){
-            activeLink.classList.remove('active');
+        activeLink.classList.remove('active');
       }
     
-      /* add class 'active' to the clicked link */
-      const activeArticles = document.querySelectorAll('.posts .post.active');
-
-      for(let activeArticle of activeArticles){
-            activeArticle.classList.remove('active');
-      }
+      /* [IN PROGRESS] add class 'active' to the clicked link */
+      clickedElement.classList.add('active');
+      console.log('clickedElement:', clickedElement);
     
-      /* remove class 'active' from all articles */
+      /* [DONE] remove class 'active' from all articles */
     
       /* get 'href' attribute from the clicked link */
     
